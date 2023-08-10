db82af9a4819a8324e519e8729771195cccaf8bc
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-08-10 12:08:39 +0200

    more expressive heartbleed example

--- src/avicenna_formalizations/heartbeat.py
+++ src/avicenna_formalizations/heartbeat.py
@@ -1,16 +1,36 @@
 import string
-from fuzzingbook.Grammars import srange, Grammar
-from fuzzingbook.Parser import EarleyParser, tree_to_string
+from fuzzingbook.Grammars import srange
 
 from avicenna.input import Input
 from avicenna.oracle import OracleResult
 
+import os
 
-grammar: Grammar = {
-    "<start>": ["<length> <payload> <padding>"],
-    "<length>": ["<one_nine><maybe_digits>"],
+
+def vulnerable_heartbeat(payload, fake_length):
+    memory = payload + str(os.urandom(100))  # Simulating extra memory after payload.
+    return memory[:fake_length]
+
+
+def oracle(test_input: Input | str):
+    heartbeat_request = str(test_input).split()
+    request_length = int(heartbeat_request[1])
+    request_payload = heartbeat_request[2]
+
+    response = vulnerable_heartbeat(request_payload, request_length)
+    if len(response) > len(request_payload):
+        return OracleResult.BUG
+    elif response == request_payload:
+        return OracleResult.NO_BUG
+    return OracleResult.UNDEF
+
+
+grammar = {
+    "<start>": ["<heartbeat-request>"],
+    "<heartbeat-request>": ["\x01 <payload-length> <payload> <padding>"],
+    "<payload-length>": ["<one_nine><maybe_digits>"],
     "<one_nine>": srange("123456789"),
-    "<maybe_digits>": ["", "<digits>"],
+    "<maybe_digits>": ["", "<digits>", ],
     "<digits>": ["<digit>", "<digit><digits>"],
     "<digit>": list(string.digits),
     "<payload>": ["<string>"],
@@ -19,32 +39,10 @@ grammar: Grammar = {
     "<char>": list(string.ascii_letters),
 }
 
-
-initial_inputs = ["3 pab x", "3 pxy xpadding", "8 pasbd xyasd"]
-
-
-def prop_(inp):
-    s = str(inp).split()
-    length = int(s[0])
-    payload_length = len(s[1])
-    if length > int(payload_length):
-        return True
-    return False
-
-
-def prop(test_input: Input):
-    s = str(test_input).split()
-    length = int(s[0])
-    payload_length = len(s[1])
-    if length > int(payload_length):
-        return OracleResult.BUG
-    return OracleResult.NO_BUG
+initial_inputs = ["\x01 5 Hello abc", "\x01 7 ILoveSE padding", "\x01 9 Hello RANDOM"]
 
 
 if __name__ == "__main__":
-    p = EarleyParser(grammar=grammar)
-
     for inp in initial_inputs:
-        for tree in p.parse(inp):
-            assert len(tree) != 0
-            assert tree_to_string(tree) == inp
+        inp_ = Input.from_str(grammar, inp, oracle(inp))
+        print(str(inp_).encode(), inp_.oracle)
