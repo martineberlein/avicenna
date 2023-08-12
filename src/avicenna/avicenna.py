438d5567d2d7681fdaa66fda64909509776bf010
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-08-12 23:43:15 +0200

    refactored avicenna class

--- src/avicenna/avicenna.py
+++ src/avicenna/avicenna.py
@@ -2,28 +2,26 @@ import logging
 import sys
 from pathlib import Path
 from time import perf_counter
-from typing import List, Dict, Set, Callable, Optional, Tuple
+from typing import List, Dict, Set, Callable, Optional, Tuple, Iterable
 
 import islearn.learner
 import pandas
 from fuzzingbook.Grammars import Grammar, is_valid_grammar
-from fuzzingbook.GrammarFuzzer import GrammarFuzzer
 from fuzzingbook.Parser import EarleyParser
 from grammar_graph.gg import GrammarGraph
 from isla.language import DerivationTree
 from isla.language import ISLaUnparser, Formula
-from isla.solver import ISLaSolver
 from islearn.learner import TruthTable as IslearnTruthTable
-
-# from sklearn.metrics import precision_score, recall_score, f1_score
 
 from avicenna import feature_extractor
 from avicenna.feature_collector import GrammarFeatureCollector
-from avicenna.generator import SimpleGenerator
+from avicenna.generator import (
+    ISLaGrammarBasedGenerator,
+    MutationBasedGenerator,
+    FuzzingbookBasedGenerator,
+)
 from avicenna.helpers import (
     Timetable,
-    register_termination,
-    CustomTimeout,
 )
 from avicenna.input import Input
 from avicenna.pattern_learner import (
@@ -37,6 +35,7 @@ from avicenna.helpers import time
 from avicenna.execution_handler import SingleExecutionHandler, BatchExecutionHandler
 from avicenna.report import SingleFailureReport, MultipleFailureReport
 from avicenna.logger import LOGGER
+from avicenna.monads import Maybe, Exceptional, check_empty
 
 ISLA_GENERATOR_TIMEOUT_SECONDS = 10
 
@@ -59,7 +58,7 @@ class Avicenna(Timetable):
         super().__init__(working_dir)
         self._start_time = None
         self._activated_patterns = activated_patterns
-        self._oracle = oracle
+        self.oracle = oracle
         self._max_iterations: int = max_iterations
         self._top_n: int = max_excluded_features - 1
         self._targeted_start_size: int = 10
@@ -88,11 +87,6 @@ class Avicenna(Timetable):
         # Islearn
         self._graph = GrammarGraph.from_grammar(grammar)
 
-        def boolean_oracle(inp_):
-            return self.map_to_bool(self._oracle(inp_))
-
-        self._boolean_oracle = boolean_oracle
-
         self.pattern_learner = AviIslearn(grammar, pattern_file=str(self._pattern_file))
 
         # TruthTable
@@ -110,9 +104,9 @@ class Avicenna(Timetable):
         )
 
         self.execution_handler = (
-            BatchExecutionHandler(self._oracle)
+            BatchExecutionHandler(self.oracle)
             if use_batch_execution
-            else SingleExecutionHandler(self._oracle)
+            else SingleExecutionHandler(self.oracle)
         )
 
         test_inputs: Set[Input] = set()
@@ -133,6 +127,7 @@ class Avicenna(Timetable):
 
         self.all_inputs: Set[Input] = test_inputs
         self.execution_handler.label(test_inputs, self.report)
+        self.best_candidates = set()
 
     @staticmethod
     def map_to_bool(result: OracleResult) -> bool:
@@ -144,77 +139,81 @@ class Avicenna(Timetable):
             case _:
                 return False
 
-    def _setup(self) -> Set[Input]:
-        """
-        This function parses the given initial input files and obtains the execution label ("BUG"/"NO_BUG")
-        :return:
-        """
-        num_failing_inputs = len(self.report.get_all_failing_inputs())
+    def generate_more_inputs(self) -> Set[Input]:
+        num_failing_inputs = self.get_num_failing_inputs()
+        result = self.get_more_inputs(num_failing_inputs)
+        return result.value() if result.is_just() else set()
 
-        filler_inputs: Set[Input] = set()
-        generator = SimpleGenerator(self.grammar)
+    def get_num_failing_inputs(self) -> int:
+        return len(self.report.get_all_failing_inputs())
+
+    def get_more_inputs(self, num_failing_inputs: int) -> Maybe[Set[Input]]:
+        generated_inputs: Set[Input] = set()
         if num_failing_inputs < self._targeted_start_size:
-            for _ in range(100):
-                inp = generator.generate()
-                filler_inputs.add(inp)
+            # generator = MutationBasedGenerator(self.grammar, self.oracle, self.all_inputs, True)
+            generator = FuzzingbookBasedGenerator(self.grammar)
+            for _ in range(20):
+                result = generator.generate()
+                if result.is_just():
+                    generated_inputs.add(result.value())
+                else:
+                    break
+        # If we have generated some inputs, we return them wrapped in a Just monad
+        if generated_inputs:
+            return Maybe.just(generated_inputs)
+        return Maybe.nothing()
 
-        return filler_inputs
-
-    @time
     def explain(self) -> List[Tuple[Formula, float, float, float]]:
-        LOGGER.info("Starting Avicenna.")
-        register_termination(self._timeout)
-        try:
-            self._start_time = perf_counter()
-            new_inputs: Set[Input] = self.all_inputs.union(self._setup())
-            while self._do_more_iterations():
-                LOGGER.info("Starting Iteration " + str(self._iteration))
-                new_inputs = self._loop(new_inputs)
-                self._iteration = self._iteration + 1
-        except CustomTimeout:
-            LOGGER.exception("Terminate due to timeout")
+        new_inputs: Set[Input] = self.all_inputs.union(self.generate_more_inputs())
+        while self._do_more_iterations():
+            LOGGER.info("Starting Iteration " + str(self._iteration))
+            new_inputs = self._loop(new_inputs)
+
         return self._finalize()
 
     def _do_more_iterations(self):
         if self._iteration >= self._max_iterations:
             logging.info("Terminate due to maximal iterations reached")
             return False
+        self._iteration += 1
         return True
 
-    @time
-    def _loop(self, test_inputs: Set[Input]):
-        # obtain labels, execute samples (Initial Step, Activity 5)
-        self.execution_handler.label(test_inputs, self.report)
+    def add_inputs(self, test_inputs: Set[Input]):
+        self.all_inputs.update(test_inputs)
+        return test_inputs
 
-        # collect features from the new samples (Activity 1)
-        for inp in test_inputs:
-            inp.features = self.collector.collect_features(inp)
-
-        # add to global list
-        valid_inputs = set([inp for inp in test_inputs if inp.oracle in (OracleResult.BUG, OracleResult.NO_BUG)])
-        self.all_inputs.update(valid_inputs)
-
-        prom, corr, excluded_features = self.feature_learner.learn(self.all_inputs)
-
-        combined_prominent_non_terminals: Set[str] = set(
-            [feature.non_terminal for feature in prom.union(corr)]
+    def construct_inputs(self, test_inputs: Set[Input]) -> Set[Input]:
+        result: Set[Input] = (
+            Exceptional.of(lambda: test_inputs)
+            .map(self.assign_label)
+            .map(lambda x: {inp for inp in x if inp.oracle != OracleResult.UNDEF})
+            .map(self.assign_feature_vector)
+            .map(self.add_inputs)
+            .reraise()
+            .get()
         )
-        exclusion_non_terminals = [
+        return result
+
+    def learn_relevant_features(self) -> List[str]:
+        """Learn prominent and correlated features."""
+        relevant, correlated, excluded_features = self.feature_learner.learn(
+            self.all_inputs
+        )
+        combined_prominent_non_terminals: Set[str] = set(
+            [feature.non_terminal for feature in relevant.union(correlated)]
+        )
+        return [
             non_terminal
             for non_terminal in self.grammar
             if non_terminal not in combined_prominent_non_terminals
         ]
-        print(combined_prominent_non_terminals)
-        print(exclusion_non_terminals)
 
-        # new_candidates, precision_truth_table, recall_truth_table = self._islearn.learn_failure_invariants(
-        #     self.all_inputs, exclusion_non_terminals
-        # )
-        (
-            new_candidates,
-            self.precision_truth_table,
-            self.recall_truth_table,
-        ) = self.pattern_learner.learn_failure_invariants(
+    def _loop(self, test_inputs: Set[Input]):
+        # first generate and give constraints not inputs
+        test_inputs = self.construct_inputs(test_inputs)
+        exclusion_non_terminals = self.learn_relevant_features()
+
+        new_candidates = self.pattern_learner.learn_failure_invariants(
             test_inputs,
             self.precision_truth_table,
             self.recall_truth_table,
@@ -223,46 +222,30 @@ class Avicenna(Timetable):
 
         new_candidates = new_candidates.keys()
 
-        LOGGER.info("Eval new stuff")
-        # Update new Candidates
-        for candidate in new_candidates:
-            self.truthTable.append(
-                TruthTableRow(candidate).set_results(
-                    *self.get_statistics(
-                        candidate,
-                        self.precision_truth_table,
-                        self.recall_truth_table,
-                    )
-                )
-            )
+        self.best_candidates = new_candidates
+        new_inputs = (
+            Exceptional.of(lambda: new_candidates)
+            # .map(self.add_negated_constraints)
+            .map(self.generate_inputs)
+            .bind(check_empty)
+            .recover(self.generate_inputs_with_grammar_fuzzer)
+            .reraise()
+            .get()
+        )
+        LOGGER.info(f"Generated {len(new_inputs)} new inputs.")
+        return new_inputs
 
-        untouched_formulas = [
-            row.formula
-            for row in self.truthTable
-            if row.formula not in set(new_candidates)
-        ]
-        for formula in untouched_formulas:
-            self.truthTable[formula].evaluate(test_inputs, self._graph)
-
-        for row in self.truthTable:
-            _, tp, fp, fn, _ = row.eval_result()
-            if (tp / (tp + fp) < 0.6) or tp / (tp + fn) < 0.9:
-                self.truthTable.remove(row)
-
-        # negate Constraints
-        # TODO
-        # 1. negate Constraints
-        # 1.2 Check for infeasible constraints
-        # 2. safe grammar graph
-        # 3. for candidate in new_candidates:
-        #   Only use top 5 constraints
-
-        # Generate new Inputs
-        test_inputs = set()
-        fuzzer = GrammarFuzzer(grammar=self.grammar)
+    def generate_inputs_with_grammar_fuzzer(self, _) -> Set[Input]:
+        generator = ISLaGrammarBasedGenerator(self.grammar)
+        generated_inputs = set()
         for _ in range(20):
-            test_inputs.add(Input(DerivationTree.from_parse_tree(fuzzer.fuzz_tree())))
-        return test_inputs
+            result_ = generator.generate()
+            if result_.is_just():
+                generated_inputs.add(result_.value())
+            else:
+                break
+        return generated_inputs
+
 
     @staticmethod
     def get_statistics(
@@ -287,39 +270,64 @@ class Avicenna(Timetable):
         logging.info("Removing infeasible constraint")
         logging.debug(f"Infeasible constraint: {constraint}")
 
-    @time
-    def _get_best_constraints(self):
-        all_constraints = []
-        for row in self.truthTable:
-            precision = row.tp / (row.tp + row.fp)
-            recall = row.tp / (row.tp + row.fn)
-            f1 = (2 * precision * recall) / (precision + recall)
-            all_constraints.append((row.formula, precision, recall, f1))
-
-    @time
     def _finalize(self) -> List[Tuple[Formula, float, float, float]]:
         logging.info("Avicenna finished")
         logging.info("The best learned failure invariant(s):")
 
-        all_constraints = []
-        for row in self.truthTable:
-            precision = row.tp / (row.tp + row.fp)
-            recall = row.tp / (row.tp + row.fn)
-            f1 = (2 * precision * recall) / (precision + recall)
-            all_constraints.append((row.formula, precision, recall, f1))
+        def meets_criteria(precision_value_, recall_value_):
+            return precision_value_ >= 0.9 and recall_value_ >= 0.6
 
-        all_constraints.sort(key=lambda x: x[3], reverse=True)
+        result = {}
+        for idx, precision_row in enumerate(self.precision_truth_table):
+            precision_value = 1 - precision_row.eval_result()
+            recall_value = self.recall_truth_table[idx].eval_result()
+
+            if meets_criteria(precision_value, recall_value):
+                result[precision_row.formula] = (precision_value, recall_value)
+
+        sorted_result = dict(
+            sorted(result.items(), key=lambda p: (p[1], -len(p[0])), reverse=True)
+        )
 
         logging.info(
             "\n".join(
                 map(
-                    lambda p: f"({p[1], p[2], p[3]}): " + ISLaUnparser(p[0]).unparse(),
-                    all_constraints[0:30],
+                    lambda x: f"({x[1][0], x[1][1]}) " + ISLaUnparser(x[0]).unparse(),
+                    sorted_result.items(),
                 )
             )
         )
+        return [
+            (candidate, stats[0], stats[1])
+            for candidate, stats in sorted_result.items()
+        ]
 
-        return all_constraints[0:5]
+    def parse_to_input(self, test_inputs: Iterable[str]) -> Set[Input]:
+        return set([Input.from_str(self.grammar, inp_) for inp_ in test_inputs])
 
-    def iteration_identifier_map(self):
-        return {"iteration": self._iteration}
+    def assign_label_single(self, test_inputs: Set[Input]) -> Set[Input]:
+        for inp_ in test_inputs:
+            inp_.oracle = self.oracle(inp_)
+        return test_inputs
+
+    def assign_label(self, test_inputs: Set[Input]) -> Set[Input]:
+        self.execution_handler.label(test_inputs)
+        return test_inputs
+
+    def assign_feature_vector(self, test_inputs: Set[Input]) -> Set[Input]:
+        collector = GrammarFeatureCollector(self.grammar)
+        for inp_ in test_inputs:
+            inp_.features = collector.collect_features(inp_)
+        return test_inputs
+
+    def generate_inputs(self, candidate_set):
+        generated_inputs = set()
+        for _ in candidate_set:
+            generator = ISLaGrammarBasedGenerator(self.grammar)
+            for _ in range(1):
+                result_ = generator.generate()
+                if result_.is_just():
+                    generated_inputs.add(result_.value())
+                else:
+                    break
+        return generated_inputs
--- src/avicenna/monads.py
+++ src/avicenna/monads.py
@@ -1,7 +1,23 @@
 from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Callable, Any, Type
+from dataclasses import dataclass
 
-T = TypeVar('T')
+
+T = TypeVar("T")
+U = TypeVar("U")
+E = TypeVar("E", bound=Exception)
+R = TypeVar("R")
+S = TypeVar("S")
+
+
+@dataclass
+class Monad(ABC, Generic[T]):
+    a: T
+
+    @abstractmethod
+    def bind(self, f: Callable[[T], "Monad[S]"]) -> "Monad[S]":
+        raise NotImplementedError()
+
 
 class Maybe(Generic[T], ABC):
     @abstractmethod
@@ -12,13 +28,34 @@ class Maybe(Generic[T], ABC):
     def is_nothing(self) -> bool:
         pass
 
+    def map(self, func) -> T:
+        raise NotImplementedError()
+
+    def bind(self, func):
+        raise NotImplementedError()
+
     @abstractmethod
     def value(self) -> T:
         pass
 
+    @classmethod
+    def just(cls, value):
+        return Just(value)
+
+    @classmethod
+    def nothing(cls):
+        return Nothing()
+
+
 class Just(Maybe[T]):
     def __init__(self, value: T):
         self._value = value
+
+    def map(self, func: Callable[[T], U]) -> "Maybe[U]":
+        return Just(func(self._value))
+
+    def bind(self, func: Callable[[T], "Maybe[U]"]) -> "Maybe[U]":
+        return func(self._value)
 
     def is_just(self) -> bool:
         return True
@@ -29,7 +66,14 @@ class Just(Maybe[T]):
     def value(self):
         return self._value
 
+
 class Nothing(Maybe):
+    def map(self, func: Callable[[T], U]) -> "Maybe[U]":
+        return self
+
+    def bind(self, func: Callable[[T], "Maybe[U]"]) -> "Maybe[U]":
+        return self
+
     def is_just(self) -> bool:
         return False
 
@@ -38,3 +82,84 @@ class Nothing(Maybe):
 
     def value(self):
         raise ValueError("No value for Nothing")
+
+
+@dataclass
+class Exceptional(Generic[E, T]):
+    @staticmethod
+    def of(f: Callable[[], T]) -> "Exceptional[E, T]":
+        try:
+            return Success(f())
+        except Exception as exc:
+            return Failure(exc)
+
+    @abstractmethod
+    def get(self) -> T:
+        pass
+
+    @abstractmethod
+    def map(self, f: Callable[[T], S]) -> "Exceptional[S]":
+        pass
+
+    @abstractmethod
+    def bind(self, f: Callable[[T], "Exceptional[E, T]"]) -> "Exceptional[E, T]":
+        raise NotImplementedError()
+
+    @abstractmethod
+    def recover(self, f: Callable[[E], T], *exc_types: Type[E]) -> "Exceptional[E, T]":
+        pass
+
+    @abstractmethod
+    def reraise(self) -> "Exceptional[T]":
+        pass
+
+
+@dataclass
+class Success(Generic[T], Exceptional[Exception, T]):
+    a: T
+
+    def get(self) -> T:
+        return self.a
+
+    def bind(self, f: Callable[[T], "Exceptional[S]"]) -> "Exceptional[S]":
+        return f(self.a)
+
+    def map(self, f: Callable[[T], S]) -> "Exceptional[S]":
+        return Exceptional.of(lambda: f(self.a))
+
+    def recover(self, _, *__) -> "Success[T]":
+        return self
+
+    def reraise(self) -> "Success[T]":
+        return self
+
+
+@dataclass
+class Failure(Generic[E], Exceptional[E, Any]):
+    a: E
+
+    def get(self) -> E:
+        raise AttributeError(f"{type(self).__name__} does not support get()")
+
+    def bind(self, _) -> "Exceptional[T]":
+        return self
+
+    def map(self, _) -> "Exceptional[S]":
+        return self
+
+    def recover(self, f: Callable[[E], T], *exc_types: Type[E]) -> "Exceptional[E, T]":
+        if not exc_types or any(isinstance(self.a, exc) for exc in exc_types):
+            return Exceptional.of(lambda: f(self.a))
+        else:
+            return self
+
+    def reraise(self) -> Exceptional[E, Any]:
+        raise self.a
+
+
+def check_empty(x: T) -> Exceptional[Exception, T]:
+    if x is None:
+        return Failure(AssertionError())
+    elif isinstance(x, (str, list, dict, set, tuple)) and not x:
+        return Failure(AssertionError())
+    return Success(x)
