e62ccccbf02a91427ccf1680f6478c9dce13a9de
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-08-15 16:46:50 +0200

    resolve deprecated pkg_resource

--- src/avicenna_formalizations/__init__.py
+++ src/avicenna_formalizations/__init__.py
@@ -1,16 +1,12 @@
-import pkg_resources
+from importlib import resources
 from pathlib import Path
 
 
 def get_pattern_file_path() -> Path:
-    return Path(
-        pkg_resources.resource_filename("avicenna_formalizations", "patterns.toml")
-    )
+    with resources.path('avicenna_formalizations', 'patterns.toml') as p:
+        return Path(p)
 
 
 def get_islearn_pattern_file() -> Path:
-    return Path(
-        pkg_resources.resource_filename(
-            "avicenna_formalizations", "patterns_islearn.toml"
-        )
-    )
+    with resources.path('avicenna_formalizations', 'patterns_islearn.toml') as p:
+        return Path(p)
