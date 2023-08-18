88b3ff08e357b6976eb4c6ffb364b2e85ee5b557
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-08-18 22:46:15 +0200

    minor

--- src/avicenna/evaluation_setup.py
+++ src/avicenna/evaluation_setup.py
@@ -11,7 +11,6 @@ from avicenna.input import Input
 
 
 class EvaluationSubject(ABC):
-
     def __init__(self, grammar, oracle, initial_inputs):
         self.grammar = grammar
         self.oracle = oracle
@@ -19,25 +18,22 @@ class EvaluationSubject(ABC):
 
     @staticmethod
     def default_param():
-        return {
-            "max_iterations": 10,
-            "timeout": 3600,
-            "log": True
-        }
+        return {"max_iterations": 10, "timeout": 3600, "log": True}
 
     def get_evaluation_config(self):
         param = self.default_param().copy()
-        param.update({
-            "grammar": self.grammar,
-            "oracle": self.oracle,
-            "initial_inputs": self.initial_inputs,
-        })
+        param.update(
+            {
+                "grammar": self.grammar,
+                "oracle": self.oracle,
+                "initial_inputs": self.initial_inputs,
+            }
+        )
         return param
 
 
 @dataclass
 class EvaluationResult:
-
     def __init__(self, subject_name, formula):
         self.subject_name = subject_name
         self.formula = formula
@@ -48,7 +44,10 @@ class EvaluationResult:
 
 def evaluate_diagnosis(grammar, evaluation_result: EvaluationResult):
     assert evaluation_result.evaluation_data
-    y_true = [1 if inp.oracle == OracleResult.BUG else 0 for inp in evaluation_result.evaluation_data]
+    y_true = [
+        1 if inp.oracle == OracleResult.BUG else 0
+        for inp in evaluation_result.evaluation_data
+    ]
 
     solver = ISLaSolver(grammar, formula=evaluation_result.formula)
     y_pred = list()
@@ -61,7 +60,9 @@ def evaluate_diagnosis(grammar, evaluation_result: EvaluationResult):
     evaluation_result.recall = tp / (tp + fn)
 
 
-def generate_evaluation_data_set(grammar, oracle, initial_inputs, targeted_size) -> List:
+def generate_evaluation_data_set(
+    grammar, oracle, initial_inputs, targeted_size
+) -> List:
     seed_inputs = set()
     for inp in initial_inputs:
         seed_inputs.add(Input.from_str(grammar, inp, None))
@@ -70,14 +71,17 @@ def generate_evaluation_data_set(grammar, oracle, initial_inputs, targeted_size)
     negative_samples = set()
 
     i = 0
-    while not (len(positive_samples) > targeted_size and len(negative_samples) > targeted_size) and i < 3:
+    while (
+        not (
+            len(positive_samples) > targeted_size
+            and len(negative_samples) > targeted_size
+        )
+        and i < 200
+    ):
         i += 1
         print(len(positive_samples), len(negative_samples))
         mutation_fuzzer = MutationBasedGenerator(
-            grammar,
-            oracle=oracle,
-            seed=seed_inputs,
-            yield_negative=True
+            grammar, oracle=oracle, seed=seed_inputs, yield_negative=True
         )
         result = mutation_fuzzer.generate()
         if result.is_just():
@@ -88,4 +92,6 @@ def generate_evaluation_data_set(grammar, oracle, initial_inputs, targeted_size)
             elif oracle_result == OracleResult.NO_BUG:
                 negative_samples.add(inp.update_oracle(OracleResult.NO_BUG))
 
-    return list(positive_samples)[:targeted_size] + list(negative_samples)[:targeted_size]
+    return (
+        list(positive_samples)[:targeted_size] + list(negative_samples)[:targeted_size]
+    )
