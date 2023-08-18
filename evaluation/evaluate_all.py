c80c253ebfcf16259eac59ed4039677d3ef3323a
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-08-18 13:51:48 +0200

    continued combined script for complete evaluation

--- evaluation/evaluate_all.py
+++ evaluation/evaluate_all.py
@@ -1,24 +1,43 @@
-from typing import List, Tuple, Callable, Set
+from typing import List, Tuple, Callable, Dict, Any
 
-from fuzzingbook.Grammars import Grammar
+from isla.language import ISLaUnparser
+from example_calculator import eval_config as calc_config
+from example_heartbleed import eval_config as heartbleed_config
+from example_middle import eval_config as middle_config
+from evaluation_setup import generate_evaluation_data_set
 
-from avicenna_formalizations.calculator import (
-    oracle as calculator_oracle,
-    initial_inputs as calculator_initial_inputs,
-    grammar as calculator_grammar
-)
-from avicenna_formalizations.heartbeat import (
-    oracle as heartbeat_oracle,
-    initial_inputs as heartbeat_initial_inputs,
-    grammar as heartbeat_grammar
-)
+from avicenna.avicenna import Avicenna
 
 if __name__ == "__main__":
+    default_param = {
+        "max_iterations": 10,
+        "timeout": 3600,
+        "log": False
+    }
 
-    subjects: List[Tuple[str ,Callable, List, Grammar]] = [
-        ("Calculator", calculator_oracle, calculator_initial_inputs, calculator_grammar),
-        ("Heartbleed", heartbeat_oracle, heartbeat_initial_inputs, heartbeat_grammar),
+    subjects: List[Tuple[str, Callable]] = [
+        ("Calculator", calc_config),
+        ("Heartbleed", heartbleed_config),
+        ("Middle", middle_config)
     ]
 
+    results = dict()
     for subject in subjects:
-        name, oracle, initial_inputs, grammar = subject
+        name, config = subject
+        config_param: Dict[str, Any] = config()
+        default_param.update(config_param)
+        print(default_param)
+
+        # measure time
+        avicenna = Avicenna(**default_param)
+        diagnosis = avicenna.explain()
+        print("Final Diagnosis:")
+        print(ISLaUnparser(diagnosis[0]).unparse())
+        results[name] = diagnosis[0]
+
+    # for subject in subjects:
+    #     name, config = subject
+    #     param = config_param
+    #     grammar = config["grammar"]
+    #
+    #     eval_data = generate_evaluation_data_set()
