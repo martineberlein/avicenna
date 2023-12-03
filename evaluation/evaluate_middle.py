3ef5706e6a1d5b863bfe4c3b5ec13ceeb92ce1a9
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-12-03 15:34:13 +0100

    fix middle eval script

--- evaluation/evaluate_middle.py
+++ evaluation/evaluate_middle.py
@@ -26,7 +26,7 @@ class MiddleSubject(EvaluationSubject):
                 "grammar": self.grammar,
                 "oracle": self.oracle,
                 "initial_inputs": self.initial_inputs,
-                "max_excluded_features": 4,
+                "top_n_relevant_features": 4,
             }
         )
         return param
