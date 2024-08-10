1e650a1d143eed22595ea7eaf8b4d19349dc1fa2
Martin Eberlein <schuldner-01.vordach@icloud.com>
2024-08-10 19:25:29 +0200

    more logging

--- src/avicenna/logger.py
+++ src/avicenna/logger.py
@@ -46,13 +46,21 @@ def learner_report(result: Optional[List[Candidate]]):
     logger.debug(f"Learned patterns: {result}")
 
 
+def relevant_feature_report(features: Optional[Set[str]]):
+    logger.info(f"Determined {features} as most relevant features.")
+
+
+def irrelevant_feature_report(features: Optional[Set[str]]):
+    logger.info(f"Excluding {features} from candidate learning.")
+
+
 def log_execution_with_report(report_func: Optional[Callable]):
     def decorator(func):
         def wrapper(*args, **kwargs):
             logger.info(f"Executing {func.__name__}")
             try:
                 result = func(*args, **kwargs)
-                logger.info(f"{func.__name__} executed successfully")
+                #logger.info(f"{func.__name__} executed successfully")
                 report_func(result)
                 return result
             except Exception as e:
@@ -67,7 +75,7 @@ def log_execution(func, report_func: Optional[Callable] = None):
         logger.info(f"Executing {func.__name__}")
         try:
             result = func(*args, **kwargs)
-            logger.info(f"{func.__name__} executed successfully")
+            # logger.info(f"{func.__name__} executed successfully")
             if report_func:
                 report_func(result)
             return result
