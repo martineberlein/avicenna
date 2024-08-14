76719c830ec80c942602ba0decf2523a1a8cb762
Martin Eberlein <schuldner-01.vordach@icloud.com>
2024-08-14 10:46:58 +0200

    adjust server eval script

--- evaluation/evaluate_server.py
+++ evaluation/evaluate_server.py
@@ -5,6 +5,7 @@ import datetime
 from isla.language import ISLaUnparser
 
 from avicenna import Avicenna
+from avicenna.learning.table import Candidate
 from debugging_benchmark.calculator.calculator import CalculatorBenchmarkRepository
 from debugging_benchmark.tests4py_benchmark.repository import (
     CalculatorBenchmarkRepository as CalculatorTests4PyBenchmarkRepository,
@@ -16,8 +17,6 @@ from debugging_benchmark.tests4py_benchmark.repository import (
 )
 from debugging_framework.benchmark.repository import BenchmarkRepository
 from debugging_framework.benchmark.program import BenchmarkProgram
-from avicenna.input import OracleResult
-from avicenna.pattern_learner import AvicennaPatternLearner
 
 
 EvalDict = {
@@ -27,16 +26,9 @@ EvalDict = {
     "middle_2": {
         "top_n_relevant_features": 3,
     },
-    "cookiecutter_2": {
-        "min_recall": .7
-    },
-    "cookiecutter_3": {
-        "min_recall": .7
-    },
-    "cookiecutter_4": {
-        "min_recall": .7
-    }
-
+    "cookiecutter_2": {"min_recall": 0.7},
+    "cookiecutter_3": {"min_recall": 0.7},
+    "cookiecutter_4": {"min_recall": 0.7},
 }
 
 
@@ -76,32 +68,25 @@ if __name__ == "__main__":
             **param,
         )
 
-        diagnosis = avicenna.explain()
-
-        program_name = str(program).replace(' ', '_')  # Adjust as needed to get a suitable filename
+        program_name = str(program).replace(" ", "_")
         output_file = os.path.join(output_dir, f"{program_name}_diagnosis.txt")
 
-        with open(output_file, 'w') as f:
+        with open(output_file, "w") as f:
             try:
-                diagnosis = avicenna.explain()
-
-                if diagnosis:
-                    f.write(f"Final Diagnosis for {program}:\n")
-                    f.write(ISLaUnparser(diagnosis[0]).unparse() + "\n\n")
+                diagnoses: List[Candidate] = avicenna.explain()
+                if diagnoses:
+                    for diagnosis in diagnoses:
+                        f.write(
+                            f"Avicenna calculated a precision of {diagnosis.precision() * 100:.2f}%, "
+                            f"recall of {diagnosis.recall() * 100:.2f}% "
+                            f"and specificity of {diagnosis.precision() * 100:.2f}%\n"
+                        )
+                        f.write(ISLaUnparser(diagnosis.formula).unparse() + "\n\n")
                 else:
                     f.write(f"No Diagnosis for {program}!\n\n")
 
             except Exception as e:
                 f.write(f"An error occurred while diagnosing {program}: {e}\n\n")
                 print(f"An error occurred while diagnosing {program}: {e}")
-
-            f.write(f"All Learned Formulas (that meet min criteria) for {program}:\n")
-            cand = avicenna.get_learned_formulas()
-            if cand:
-                for can in cand:
-                    f.write(f"Avicenna calculated a precision of {can[1] * 100:.2f}% and a recall of {can[2] * 100:.2f}%\n")
-                    f.write(ISLaUnparser(can[0]).unparse() + "\n\n")
-            else:
-                f.write("No learned formulas were found.\n")
 
         print(f"Diagnosis saved to {output_file}")
